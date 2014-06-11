f816b354e5919494f6bcb9610d692c6881ca695c
plutoattack <plutoattackme@gmail.com>
2014-06-11 13:20:09 +0300

    saving grades for sudent in db

--- grails-app/controllers/com/meerkat/school/CalculateController.groovy
+++ grails-app/controllers/com/meerkat/school/CalculateController.groovy
@@ -3,17 +3,31 @@ package com.meerkat.school
 class CalculateController {
 
     def index() {
-//        def student=new Student()
-//        student.url=
+        def student=new Student()
         def generator = { String alphabet, int n ->
             new Random().with {
                 (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
             }
         }
-        println generator( (('A'..'Z')+('0'..'9')).join(), 9 )
+        student.url=generator( (('A'..'Z')+('0'..'9')).join(), 9 )
+        student.save(flush: true)
         Course.all.each {c->
-//            String p='grade'+c.id
-//            println params.get(p)
+            String p='grade'+c.id
+            if(params.get(p)){
+               println 'in'
+               def g=new Grade()
+               g.course=c
+               g.student=student
+               g.final_grade=params.get(p).toInteger()
+                if(g.validate()){
+                    g.save(flush: true)
+                }else{
+                    g.errors.allErrors.each {
+                        println it
+                    }
+                }
+
+            }
         }
     }
 }
