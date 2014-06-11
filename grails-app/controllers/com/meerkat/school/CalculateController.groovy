e5576a583a44ecb2e6d27af63facfe0ce54bb3b9
plutoattack <plutoattackme@gmail.com>
2014-06-11 03:18:43 +0300

    random string generator

--- grails-app/controllers/com/meerkat/school/CalculateController.groovy
+++ grails-app/controllers/com/meerkat/school/CalculateController.groovy
@@ -3,11 +3,17 @@ package com.meerkat.school
 class CalculateController {
 
     def index() {
-        def student=new Student()
-        student.url=
+//        def student=new Student()
+//        student.url=
+        def generator = { String alphabet, int n ->
+            new Random().with {
+                (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+            }
+        }
+        println generator( (('A'..'Z')+('0'..'9')).join(), 9 )
         Course.all.each {c->
-            String p='grade'+c.id
-            println params.get(p)
+//            String p='grade'+c.id
+//            println params.get(p)
         }
     }
 }
